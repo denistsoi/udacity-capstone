1a90d607dd2741ab731ac821910a1cd0c78f0770
Denis Tsoi <denistsoi@gmail.com>
2020-05-05 11:33:09 +0800

    ADD: healthy route;

--- app.py
+++ app.py
@@ -3,12 +3,18 @@ from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
-def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__)
-  CORS(app)
 
-  return app
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__)
+    CORS(app)
+
+    @app.route("/")
+    def home():
+        return "healthy"
+
+    return app
+
 
 app = create_app()
 
